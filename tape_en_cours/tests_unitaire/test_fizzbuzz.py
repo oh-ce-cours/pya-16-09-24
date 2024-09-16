1d744e4546c8adc04b0342cabc020c22704a49d0
Matthieu Falce <matthieu@falce.net>
2024-09-16 16:49:50 +0200

    gitautosync update 40

--- tape_en_cours/tests_unitaire/test_fizzbuzz.py
+++ tape_en_cours/tests_unitaire/test_fizzbuzz.py
@@ -2,7 +2,7 @@ from fizzbuzz import is_multiple
 
 
 def test_multiple():
-    assert is_multiple(3, 5) == False
+    assert not is_multiple(3, 5)
     assert is_multiple(3, 3) == True
     assert is_multiple(5, 5) == True
     assert is_multiple(5, 3) == False
