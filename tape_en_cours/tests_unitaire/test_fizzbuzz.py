a86d7dea97e8e042571379ebce891bde10282f4d
Matthieu Falce <matthieu@falce.net>
2024-09-17 11:08:21 +0200

    gitautosync update 93

--- tape_en_cours/tests_unitaire/test_fizzbuzz.py
+++ tape_en_cours/tests_unitaire/test_fizzbuzz.py
@@ -8,41 +8,41 @@ def test_multiple():
     assert not is_multiple(5, 3)
 
 
-def test_fizzbuzz_multiple_3_est_fizz():
-    # arange
-    nombre = 3
-    attendu = "fizz"
-    # act
-    result = regle_fizz_buzz(nombre)
-    # assert
-    assert result == attendu
+# def test_fizzbuzz_multiple_3_est_fizz():
+#     # arange
+#     nombre = 3
+#     attendu = "fizz"
+#     # act
+#     result = regle_fizz_buzz(nombre)
+#     # assert
+#     assert result == attendu
 
 
-def test_fizzbuzz_multiple_5_est_buzz():
-    # arange
-    nombre = 5
-    attendu = "buzz"
-    # act
-    result = regle_fizz_buzz(nombre)
-    # assert
-    assert result == attendu
+# def test_fizzbuzz_multiple_5_est_buzz():
+#     # arange
+#     nombre = 5
+#     attendu = "buzz"
+#     # act
+#     result = regle_fizz_buzz(nombre)
+#     # assert
+#     assert result == attendu
 
 
-def test_fizzbuzz_multiple_30_est_fizzbuzz():
-    # arange
-    nombre = 30
-    attendu = "fizzbuzz"
-    # act
-    result = regle_fizz_buzz(nombre)
-    # assert
-    assert result == attendu
+# def test_fizzbuzz_multiple_30_est_fizzbuzz():
+#     # arange
+#     nombre = 30
+#     attendu = "fizzbuzz"
+#     # act
+#     result = regle_fizz_buzz(nombre)
+#     # assert
+#     assert result == attendu
 
 
-def test_fizzbuzz_non_multiple_est_nombre():
-    # arange
-    nombre = 4
-    attendu = "4"
-    # act
-    result = regle_fizz_buzz(nombre)
-    # assert
-    assert result == attendu
+# def test_fizzbuzz_non_multiple_est_nombre():
+#     # arange
+#     nombre = 4
+#     attendu = "4"
+#     # act
+#     result = regle_fizz_buzz(nombre)
+#     # assert
+#     assert result == attendu
