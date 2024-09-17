a539f85451defcbf16cddea81bdc684d276de0e6
Matthieu Falce <matthieu@falce.net>
2024-09-17 14:20:35 +0200

    gitautosync update 206

--- tape_en_cours/tests_unitaire/test_plus_moins.py
+++ tape_en_cours/tests_unitaire/test_plus_moins.py
@@ -37,7 +37,7 @@ def test_nombre_propose_egal_cible():
 def test_dis_la_phrase_cest_moins():
     # arange
     position = plus_moins.Position.PLUS.value
-    attendu = "Le nombre a trouver est plus petit"
+    attendu = plus_moins.Phrases.PLUS.value
     # act
     resultat = plus_moins.dis_la_phrase(position)
     # assert
