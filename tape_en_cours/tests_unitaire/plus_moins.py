d4f8c8e92e41cf02d2af31e67c14c4ffd81f2b45
Matthieu Falce <matthieu@falce.net>
2024-09-17 13:59:19 +0200

    gitautosync update 150

--- tape_en_cours/tests_unitaire/plus_moins.py
+++ tape_en_cours/tests_unitaire/plus_moins.py
@@ -1,2 +1,5 @@
 def regle_du_jeu(nombre_propose: int, nombre_a_deviner: int) -> int:
-    return -1
+    if nombre_propose > nombre_a_deviner:
+        return 1
+    elif nombre_propose < nombre_a_deviner:
+        return -1
