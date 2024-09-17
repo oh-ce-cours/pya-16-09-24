254f89b6608eb4aea61c5200961ea5528e051741
Matthieu Falce <matthieu@falce.net>
2024-09-17 10:54:05 +0200

    gitautosync update 74

--- tape_en_cours/tests_unitaire/fizzbuzz.py
+++ tape_en_cours/tests_unitaire/fizzbuzz.py
@@ -13,6 +13,6 @@ def regle_fizz_buzz(nombre):
     return res
 
 
-for nombre in range(1, 16):
-    res = regle_fizz_buzz(nombre)
-    print(res)
+# for nombre in range(1, 16):
+#     res = regle_fizz_buzz(nombre)
+#     print(res)
