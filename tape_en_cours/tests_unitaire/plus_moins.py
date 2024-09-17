ac9524ef4aef161e1276af0c2d436ef8004585ec
Matthieu Falce <matthieu@falce.net>
2024-09-17 14:14:29 +0200

    gitautosync update 185

--- tape_en_cours/tests_unitaire/plus_moins.py
+++ tape_en_cours/tests_unitaire/plus_moins.py
@@ -24,8 +24,6 @@ def game():
         nombre_propose = int(input("Entrez un nombre : "))
         resultat = regle_du_jeu(nombre_propose, cible)
         print(dis_la_phrase(resultat))
-        if resultat == 0:
-            break
 
 
 def main():
