08d62fb7c606fd5edadffcb55b658b733ad2c52b
Matthieu Falce <matthieu@falce.net>
2024-09-17 13:56:59 +0200

    gitautosync update 144

--- tape_en_cours/tests_unitaire/test_plus_moins.py
+++ tape_en_cours/tests_unitaire/test_plus_moins.py
@@ -2,11 +2,11 @@ import plus_moins
 
 
 def test_nombre_propose_plus_grand_que_cible():
-    # arange 
+    # arange
     nombre_propose = 4
     nombre_a_deviner = 10
     attendu = -1
-    # act 
+    # act
     resultat = plus_moins.regle_du_jeu(nombre_propose, nombre_a_deviner)
-    # assert 
-    assert 
+    # assert
+    assert resultat == attendu
