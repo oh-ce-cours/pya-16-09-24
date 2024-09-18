87dd9afa60ee0669f304649f9e3486095d231778
Matthieu Falce <matthieu@falce.net>
2024-09-18 09:19:17 +0200

    gitautosync update 281

--- tape_en_cours/parsing_example_mp.py
+++ tape_en_cours/parsing_example_mp.py
@@ -108,8 +108,8 @@ lines = tle_data.split("\n")
 lines = [l.strip() for l in lines if l]
 items = list(chunks(lines, 3))
 
-print(f"Il y a {len(items)} items à parser")
 
 if __name__ == "__main__":
+    print(f"Il y a {len(items)} items à parser")
     with multiprocessing.Pool(15) as p:
         print(p.map(parsing_triplet, items))
