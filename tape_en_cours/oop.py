d6e90f1155f72f53ca402203f457bd590a3e4329
Matthieu Falce <matthieu@falce.net>
2024-09-19 11:35:12 +0200

    gitautosync update 401

---
+++ tape_en_cours/oop.py
@@ -0,0 +1,24 @@
+class Mere:
+    def methode(self):
+        print("dans mere methode 1")
+
+    def methode2(self):
+        print("dans mere methode 2")
+
+
+class Fille(Mere):
+    def methode(self):
+        print("dans fille methode 1")
+        self.methode2()
+
+    def methode2(self):
+        print("dans fille methode 2")
+
+
+f = Fille()
+print(type(f))
+print(isinstance(f, Fille), isinstance(f, Mere))
+
+m = Mere()
+print(type(m))
+print(isinstance(m, Fille), isinstance(m, Mere))
